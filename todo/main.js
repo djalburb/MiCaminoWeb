e762d9073096dfe58ef30027edfd2f51e11febfc
djalburb <djalburb@gmail.com>
2021-04-23 11:12:01 -0600

    Validacion de Campo

--- todo/main.js
+++ todo/main.js
@@ -1,8 +1,11 @@
 let todos = [];
+
+// Cargamos Datos almacenados en el LocalStorage
 todos = JSON.parse(localStorage.getItem('todos'))
 
 const errorMessage = document.getElementById('errorMessage')
 
+//Renderizamos los datos
 const render = () => {
     const todoList = document.getElementById('todo-list')
     const todoTempleate = todos.map(t => `<li>${t}</li>`)
@@ -24,11 +27,10 @@ window.onload = () => {
     render();
 
     const form = document.getElementById('todo-form')
-    // Agregando elementos
+    const todo = document.getElementById('todo')
 
     form.onsubmit = (e) => {
         e.preventDefault()
-        const todo = document.getElementById('todo')
         const todoText = todo.value;
         todo.value = ''
         if (todoText.trim() == ""){
@@ -39,6 +41,12 @@ window.onload = () => {
         render();
         closePopup();
     }
+
+    todo.addEventListener('keypress', () => {
+        if (errorMessage.textContent != ""){
+            errorMessage.innerText = ''
+        }
+    })
 
 
     // Popup
